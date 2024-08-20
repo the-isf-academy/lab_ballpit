c38c7a64c0f8991a58d0ba0b6fe5b1cc994ce04e
brittegenzlinger <britte.genzlinger@gmail.com>
2024-08-20 15:25:24 +0800

    snake case

--- ball.py
+++ ball.py
@@ -14,8 +14,8 @@ class Ball():
         self.x = random.random()
         self.y = random.random()
         
-        self.xVel = random.uniform(.0001,.001)
-        self.yVel = random.uniform(.0001,.001)
+        self.x_vel = random.uniform(.0001,.001)
+        self.y_vel = random.uniform(.0001,.001)
         
         self.size = random.randint(1,10)
 
@@ -42,17 +42,17 @@ class Ball():
         # to immitate a "bounce"
 
         if self.x < 0 or self.x > 1:
-            self.xVel = self.xVel*-1
+            self.x_vel = self.x_vel*-1
         if self.y < 0 or self.y > 1:
-            self.yVel = self.yVel*-1
+            self.y_vel = self.y_vel*-1
 
     def move(self):
         # Moves the ball slightly along its current path
        
 
         self.turtle.up()
-        self.x += self.xVel
-        self.y += self.yVel
+        self.x += self.x_vel
+        self.y += self.y_vel
         self.set_position(self.x,self.y)
 
     def set_color(self):
